8fa03de67126fad169e1325e672f1462b26b94dc
Karol Urbańczyk <karolurb@gmail.com>
2015-09-09 14:04:53 +0200

    Add service provider for sqlite database

--- src/app.php
+++ src/app.php
@@ -2,9 +2,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../db/worldcup.db',
+    ),
+));
 
 $app->get('/hello/{name}', function ($name) use ($app) {
-  return 'Hello '.$app->escape($name);
+    return 'Hello '.$app->escape($name);
 });
 
 return $app;
